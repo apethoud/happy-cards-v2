b345dce277e8e15d169b71b7abf5897daf89762c
Andrew Pethoud <andrewpethoud@gmail.com>
2022-06-17 15:48:18 -0700

    add site header and footer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import "./App.css";
+import HeaderFooter from "./components/HeaderFooter";
 import Home from "./components/Home";
 
 function App() {
-  return <Home />;
+  return (
+    <HeaderFooter>
+      <Home />
+    </HeaderFooter>
+  );
 }
 
 export default App;
---
+++ src/components/HeaderFooter.js
@@ -0,0 +1,12 @@
+export default function HeaderFooter(props) {
+  return (
+    <div>
+      <div className="HeaderFooterColorBar" />
+      <div style={{ padding: 10 }}>
+        <div className="Logo">HappyCards</div>
+      </div>
+      <div style={{ padding: 40 }}>{props.children}</div>
+      <div className="Copyright">&copy; 2022 Andrew Pethoud</div>
+    </div>
+  );
+}
