a43db0d682b7d154f96dab328b688160f3a80d98
Andrew Pethoud <andrewpethoud@gmail.com>
2022-06-25 19:19:23 -0700

    feat(CardForm): refactored form to use styled components for all components with visual styling

--- src/components/HeaderFooter.js
+++ src/components/HeaderFooter.js
@@ -1,12 +1,14 @@
+import { AppBackground } from "./StyledComponents";
+
 export default function HeaderFooter(props) {
   return (
-    <div>
-      <div className="HeaderFooterColorBar" />
+    <AppBackground>
+      <div className="HeaderColorBar" />
       <div style={{ padding: 10 }}>
         <div className="Logo">HappyCards</div>
       </div>
       <div style={{ padding: 40 }}>{props.children}</div>
       <div className="Copyright">&copy; 2022 Andrew Pethoud</div>
-    </div>
+    </AppBackground>
   );
 }
---
+++ src/components/StyledComponents.js
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+
+export const AppBackground = styled.div`
+  background-color: #fff;
+`;
+
+export const Button = styled.button`
+  border-radius: 6px;
+  margin-right: 8px;
+  padding: 8px;
+  font-size: 16px;
+  font-weight: 700;
+  border: 2px solid #ddd;
+  background-color: #ddd;
+  color: #333;
+  ${(props) =>
+    props.primary
+      ? `
+        border: 2px solid darkturquoise;
+        background-color: darkturquoise;
+        color: #fff;
+        `
+      : `
+        border: 2px solid #ddd;
+        background-color: #ddd;
+        color: #333;
+        `}
+`;
+
+export const Label = styled.label`
+  font-size: 16px;
+  font-weight: 700;
+  margin-bottom: 8px;
+`;
+
+export const Input = styled.input`
+  border: 1px solid #ccc;
+  padding: 8px;
+  font-size: 16px;
+`;
