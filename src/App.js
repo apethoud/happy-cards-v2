9337d2f5d720c78bc91e7dc3b14d2ca21f57f095
Andrew Pethoud <andrewpethoud@gmail.com>
2022-07-23 17:20:40 -0700

    add basic routing for Home and CardBuilder components

--- src/App.js
+++ src/App.js
@@ -3,14 +3,19 @@ import HeaderFooter from "./components/HeaderFooter";
 import Home from "./components/Home";
 import CardBuilder from "./components/CardBuilder";
 import { ThemeProvider } from "./contexts/ThemeContext";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <ThemeProvider>
-      <HeaderFooter>
-        <Home />
-        <CardBuilder />
-      </HeaderFooter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HeaderFooter />}>
+            <Route index element={<Home />} />
+            <Route path="card-builder" element={<CardBuilder />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
--- src/components/HeaderFooter.js
+++ src/components/HeaderFooter.js
@@ -8,6 +8,7 @@ import {
 } from "./StyledComponents";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSun, faMoon } from "@fortawesome/free-solid-svg-icons";
+import { Outlet } from "react-router-dom";
 
 export default function HeaderFooter(props) {
   const { theme, toggleTheme } = useContext(ThemeContext);
@@ -24,7 +25,9 @@ export default function HeaderFooter(props) {
           />
         </div>
       </div>
-      <div style={{ padding: 40 }}>{props.children}</div>
+      <div style={{ padding: 40 }}>
+        <Outlet />
+      </div>
       <FooterCopyright>&copy; 2022 Andrew Pethoud</FooterCopyright>
     </AppBackground>
   );
